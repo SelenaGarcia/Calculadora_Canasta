88f6fc1beae13142df94b6ae3572b94ea25c12c6
Selena Garcia <selenagarcialobo@ieee.org>
2021-06-08 00:20:37 -0300

    calculo

--- src/components/CalculadoraCanasta.vue
+++ src/components/CalculadoraCanasta.vue
@@ -1,6 +1,6 @@
 <template>
   <form-wizard
-    color="#e67e22"
+    color="#0d74bf"
     back-button-text="Anterior"
     next-button-text="Siguiente"
     finish-button-text="Volver al inicio"
@@ -34,8 +34,8 @@
     </tab-content>
     <tab-content title="Resultado para tu hogar">
       Cantidad de miembros en el hogar Canasta básica general
-      <strong>{{ canastaBasicaGeneral }} </strong> Canasta básica alimentaria
-      <strong>{{ canastaBasicaAlimentaria }}</strong>
+      <strong>${{ canastaBasicaGeneral }} </strong> Canasta básica alimentaria
+      <strong>${{ canastaBasicaAlimentaria }}</strong>
 
       Si los ingresos de tu hogar se encuentran por debajo de los
       {{ canastaBasicaGeneral }} está por debajo de la línea de la pobreza: por
@@ -74,46 +74,151 @@ export default {
         indice = indice + this.calcularIndice(this.edades[i], this.sexos[i]);
       }
 
-      console.log(indice);
-      this.canastaBasicaGeneral = indice * 10000;
-      this.canastaBasicaAlimentaria = indice * 200000;
+      var numbForm = new Intl.NumberFormat("es-ES" );
+      this.canastaBasicaGeneral = numbForm.format(indice * 17390.47);
+      this.canastaBasicaAlimentaria = numbForm.format(indice * 7833.55);
     },
     calcularIndice(edad, sexo) {
-      if (edad <= 0) {
-        return 0.35;
+      if (edad < 0) {
+        return 0;
       }
       if (edad < 1) {
+        if (sexo == "Femenino") {
+          return 0.35;
+        }
+        return 0.35;
+      }
+      if (edad <= 1) {
+        if (sexo == "Femenino") {
+          return 0.37;
+        }
         return 0.37;
       }
-      if (edad < 2) {
+      if (edad <= 2) {
+        if (sexo == "Femenino") {
+          return 0.46;
+        }
         return 0.46;
       }
-      if (edad < 3) {
+      if (edad <= 3) {
+        if (sexo == "Femenino") {
+          return 0.51;
+        }
         return 0.51;
       }
-      if (edad < 4) {
+      if (edad <= 4) {
+        if (sexo == "Femenino") {
+          return 0.55;
+        }
         return 0.55;
       }
-      if (edad < 5) {
+      if (edad <= 5) {
+        if (sexo == "Femenino") {
+          return 0.6;
+        }
         return 0.6;
       }
-      if (edad < 6) {
+      if (edad <= 6) {
+        if (sexo == "Femenino") {
+          return 0.64;
+        }
         return 0.64;
       }
-      if (edad < 7) {
+      if (edad <= 7) {
+        if (sexo == "Femenino") {
+          return 0.66;
+        }
         return 0.66;
       }
-      if (edad < 8) {
+      if (edad <= 8) {
+        if (sexo == "Femenino") {
+          return 0.68;
+        }
         return 0.68;
       }
-      if (edad < 9) {
+      if (edad <= 9) {
+        if (sexo == "Femenino") {
+          return 0.69;
+        }
         return 0.69;
       }
-      if (edad < 9) {
+      if (edad <= 10) {
         if (sexo == "Femenino") {
-          return 0.79;
+          return 0.7;
         }
-        return 0.7;
+        return 0.79;
+      }
+      if (edad <= 11) {
+        if (sexo == "Femenino") {
+          return 0.72;
+        }
+        return 0.82;
+      }
+      if (edad <= 12) {
+        if (sexo == "Femenino") {
+          return 0.74;
+        }
+        return 0.85;
+      }
+      if (edad <= 13) {
+        if (sexo == "Femenino") {
+          return 0.76;
+        }
+        return 0.9;
+      }
+      if (edad <= 14) {
+        if (sexo == "Femenino") {
+          return 0.76;
+        }
+        return 0.96;
+      }
+      if (edad <= 15) {
+        if (sexo == "Femenino") {
+          return 0.77;
+        }
+        return 1.0;
+      }
+      if (edad <= 16) {
+        if (sexo == "Femenino") {
+          return 0.77;
+        }
+        return 1.03;
+      }
+      if (edad <= 17) {
+        if (sexo == "Femenino") {
+          return 0.77;
+        }
+        return 1.04;
+      }
+      if (edad <= 29) {
+        if (sexo == "Femenino") {
+          return 0.76;
+        }
+        return 1.02;
+      }
+      if (edad <= 45) {
+        if (sexo == "Femenino") {
+          return 0.77;
+        }
+        return 1.0;
+      }
+      if (edad <= 60) {
+        if (sexo == "Femenino") {
+          return 0.76;
+        }
+        return 1.0;
+      }
+      if (edad <= 75) {
+        if (sexo == "Femenino") {
+          return 0.67;
+        }
+        return 0.83;
+      }
+      if (edad <= 999) {
+        if (sexo == "Femenino") {
+          return 0.63;
+        }
+        return 0.74;
       }
     },
   },
